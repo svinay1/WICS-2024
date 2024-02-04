adc2ba0b01272a738e170ae4c39a692d6a6b87a3
Samhita Vinay <samhitavinay@gmail.com>
2024-02-04 05:12:00 -0600

    More formatting changes

--- frontend/src/Resources.js
+++ frontend/src/Resources.js
@@ -3,12 +3,20 @@ import { useState, useEffect } from 'react';
 
 const Resources = () => {
     const [currAdvice, setCurrAdvice] = useState(1);
+    const [text1, setText1] = useState();
+    const [text2, setText2] = useState();
+    const [text3, setText3] = useState();
   
     const changeAdvice = () => {
         if (currAdvice) {
-            console.log("budgeting");
+            setText1("Track Your Expenses: Start by keeping track of all your expenses over a month. Notice any patterns?");
+            setText2("Set Clear Goals: Establish short-term and long-term financial goals to guide your budgeting efforts.");
+            setText3("Prioritize and Adjust: Prioritize your needs first when allocating funds in your budget.");
             setCurrAdvice(false);
       } else {
+            setText1("Diversify Your Portfolio: Spread your investments across different asset classes to reduce risk.");
+            setText2("Invest for the Long Term: Instead of trying to time the market, focus on holding quality investments.");
+            setText3("Educate Yourself: Take the time to learn about investment options, strategies, and risk factors. ");
         setCurrAdvice(true);
       }
     };
@@ -21,13 +29,13 @@ const Resources = () => {
                     <button onClick={changeAdvice} type="button">Investing</button>
                 </div>
                 <header className="tips">
-                <h2>a tip</h2>
+                <h2>{text1}</h2>
                 </header>
                 <header className="tips">
-                <h2>a tip</h2>
+                <h2>{text2}</h2>
                 </header>
                 <header className="tips">
-                <h2>a tip</h2>
+                <h2>{text3}</h2>
                 </header>
             </header>
     );
