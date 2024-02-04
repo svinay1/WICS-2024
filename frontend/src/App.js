662e754218feeb6ae1e532f7194e646b5a2b2068
tr-c <tc82@rice.edu>
2024-02-03 18:19:52 -0600

    pre-routing install, app page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,15 +12,18 @@ function App() {
           <ul>
             <li><a href="http://localhost:3000/">Home</a></li>
             <li><a href="https://google.com/">Stocks</a></li>
-            <li><a href="#">Tips</a></li>
+            <li><a href="http://localhost:3001/">Tips</a></li>
           </ul>
         </nav>
       </header>
-      {/* image of the barbie head */}
-      <h1>Hi Barbie!</h1>
+      {/* image of the barbie logo */}
+      <h1>Hi Barbie!</h1> 
       <h2> Let's do some girl math.</h2>
       {/* <MyComponent /> */}
-
+      <header className="missionBox"> 
+        <h3>Our Mission</h3>
+        <h2>Improve financial literacy of women</h2>
+      </header>
       {/* hey */}
     </div>
   );
