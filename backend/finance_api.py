4c0f01ef570b6a6b94425e010b14d6229e0c0d11
Samhita Vinay <samhitavinay@gmail.com>
2024-02-03 23:48:15 -0600

    Added all functions for DB

--- backend/finance_api.py
+++ backend/finance_api.py
@@ -1,18 +1,41 @@
 import yfinance as yf
 
-def get_stock_price(symbol):
+def get_stock_price(symbol, period):
     try:
         stock = yf.Ticker(symbol)
-        current_price = stock.history(period='1m')['Close'].iloc[-1]
+        current_price = stock.history(period=period)['Close'].iloc[-1]
         #historical_prices = stock.history(period='1m')
         return current_price
     except Exception as e:
         print(f"Error: {e}")
         return None
     
-def get_sponsor_stock_prices(symbols):
+def get_stock_price_over_time(symbol):
+    try:
+        stock_data_unrounded = yf.download(symbol, start='2024-01-04', end='2024-02-04')['Close'].tolist()
+        rounded_list = []
+        idx = 0
+        for data in stock_data_unrounded:
+            rounded_list.append((idx, round(data, 2)))
+            idx += 1
+        return rounded_list
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    
+def get_sponsor_stock_prices_current(symbols, period):
     output = {}
     for symbol in symbols:
-        output[symbol] = round(get_stock_price(symbol), 2)
+        output[symbol] = round(get_stock_price(symbol, period), 2)
 
     return output
+
+def get_sponsor_stock_prices_over_time(symbols):
+    output = {}
+    for idx in range(len(symbols)):
+        output[symbols[idx]] = get_stock_price_over_time(symbols[idx])
+
+    return output
+
+print(list(range(1, 31)))
--- backend/flask_api.py
+++ backend/flask_api.py
@@ -1,26 +1,35 @@
 from flask import Flask, request
-from finance_api import get_stock_price, get_sponsor_stock_prices
+from finance_api import get_sponsor_stock_prices_current, get_sponsor_stock_prices_over_time
 import json
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/stock-history', methods=['GET'])
-def stock_history():
+@app.route('/current-price', methods=['GET'])
+def current_stock_price():
     #symbol = request.args.get('symbol', default=None, type=str)
     #start_date = request.args.get('start_date', default=None, type=str)
     #end_date = request.args.get('end_date', default=str(datetime.today().date()), type=str)
 
-    #if symbol is None:
-        #return json.dumps({'error': 'Invalid parameters. Please provide symbol and start_date.'}, indent = 4), 400
-
-    #historical_prices = get_stock_price(symbol)
-
-    current_prices = get_sponsor_stock_prices(['BP', 'CVX', 'DKNG', 'HD', 'LMT', 'GS', 'GOOG', 'META', 'RBLX', 'TEAM'])
+    current_prices = get_sponsor_stock_prices_current(['BP', 'CVX', 'DKNG', 'HD', 'LMT', 'GS', 'GOOG', 'META', 'RBLX', 'TEAM'], "1d")
 
     if current_prices is not None:
         return json.dumps(current_prices, indent = 4), 200
+    else:
+        return json.dumps({'error': f"Unable to retrieve current stock prices"}, indent = 4), 500
+
+
+@app.route('/price-over-time', methods=['GET'])
+def price_over_time():
+    #symbol = request.args.get('symbol', default=None, type=str)
+    #start_date = request.args.get('start_date', default=None, type=str)
+    #end_date = request.args.get('end_date', default=str(datetime.today().date()), type=str)
+
+    prices_over_time = get_sponsor_stock_prices_over_time(['BP', 'CVX', 'DKNG', 'HD', 'LMT', 'GS', 'GOOG', 'META', 'RBLX', 'TEAM'])
+
+    if prices_over_time is not None:
+        return json.dumps(prices_over_time, indent = 4), 200
     else:
         return json.dumps({'error': f"Unable to retrieve current stock prices"}, indent = 4), 500
 
